ccfc39c9b2aa73998f6246386c1d850898df6a76
paulccampos <paulccampos@gmail.com>
2025-09-12 16:05:14 +0800

    Add files via upload

---
+++ spoti/demo/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.example to javafx.fxml;
+    opens com.example.controllers to javafx.fxml;
+    exports com.example;
+}
