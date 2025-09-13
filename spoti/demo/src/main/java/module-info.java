fd0fbe7ee7f4244d6247c89d3eac10dfc0c5d68d
paulccampos <paulccampos@gmail.com>
2025-09-13 23:29:04 +0800

    Delete spoti directory

--- spoti/demo/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.example to javafx.fxml;
-    opens com.example.controllers to javafx.fxml;
-    exports com.example;
-}
